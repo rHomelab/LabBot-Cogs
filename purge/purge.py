b956bb9b836ec681d7ebc4323a8f3eab27acc7a3
Keir Davis <keirjdavis@gmail.com>
2020-02-25 10:30:54 +0000

    feat(purge): add minumum age to embed

--- purge/purge.py
+++ purge/purge.py
@@ -14,7 +14,7 @@ class PurgeCog(commands.Cog):
 
         default_guild_settings = {
             "purge_excludedusers": [],
-            "purge_limit": 5,
+            "purge_minage": 5,
             "purge_schedule": "",
             "purge_count": 0,
             "purge_lastrun": 0,
@@ -58,8 +58,8 @@ class PurgeCog(commands.Cog):
             if len(roles) > 0:
                 continue
 
-            # If user is not older than the limit, they're safe
-            timelimit = await self.settings.guild(guild).purge_limit()
+            # If user is not older than the minimum age, they're safe
+            timelimit = await self.settings.guild(guild).purge_minage()
             cutoff_date = datetime.utcnow() - timedelta(days=timelimit)
             if member.joined_at > cutoff_date:
                 continue
@@ -139,16 +139,16 @@ class PurgeCog(commands.Cog):
         else:
             await ctx.send("That user is already not safe from pruning!")
 
-    @_purge.command("setlimit")
-    async def purge_setlimit(self, ctx: commands.Context, limit: int):
+    @_purge.command("setminage")
+    async def purge_setminage(self, ctx: commands.Context, minage: int):
         """Sets the number of days a user can remain in the server with no
         roles before being purged.
 
         Example:
-        - `[p]purge setlimit <days>`
+        - `[p]purge setminage <days>`
         """
-        await self.settings.guild(ctx.guild).purge_limit.set(limit)
-        await ctx.send(f"Set the new limit to {limit} days.")
+        await self.settings.guild(ctx.guild).purge_minage.set(minage)
+        await ctx.send(f"Set the new minimum age to {minage} days.")
 
     @_purge.command("schedule")
     async def purge_schedule(self, ctx: commands.Context):
@@ -194,10 +194,12 @@ class PurgeCog(commands.Cog):
         """
         purge_count = await self.settings.guild(ctx.guild).purge_count()
         purge_enabled = await self.settings.guild(ctx.guild).purge_enabled()
+        purge_minage = await self.settings.guild(ctx.guild).purge_minage()
 
         data = discord.Embed(colour=(await ctx.embed_colour()))
         data.add_field(name="Purged", value=f"{purge_count}")
         data.add_field(name="Enabled", value=f"{purge_enabled}")
+        data.add_field(name="Min Age", value=f"{purge_minage}")
 
         try:
             await ctx.send(embed=data)
