337ed871494e9d74a9d1e1a593fea5829704483c
Keir Davis <keirjdavis@gmail.com>
2020-02-26 17:22:12 +0000

    feat(verify): add status command

--- verification/verification.py
+++ verification/verification.py
@@ -1,4 +1,5 @@
 """discord red-bot verification"""
+import discord
 from redbot.core import commands, checks, Config
 
 
@@ -33,3 +34,27 @@ class VerificationCog(commands.Cog):
         """
         await self.settings.guild(ctx.guild).verify_message.set(message)
         await ctx.send("Verify message set.")
+
+    @_verify.command("status")
+    async def verify_status(self, ctx: commands.Context):
+        """Status of the bot.
+        The bot will display how many users it has verified
+        since it's inception.
+        In addition, will also post its current configuration and status.
+
+        Example:
+        - `[p]verify status`
+        """
+        verify_message = await self.settings.guild(ctx.guild).verify_message()
+        verify_message = verify_message.replace('`', '')
+        verify_count = await self.settings.guild(ctx.guild).verify_count()
+
+        data = discord.Embed(colour=(await ctx.embed_colour()))
+        data.add_field(name="Verified", value=f"{verify_count} users")
+        data.add_field(name="Message", value=f"`{verify_message}`")
+
+        try:
+            await ctx.send(embed=data)
+        except discord.Forbidden:
+            await ctx.send("I need the `Embed links` permission to " +
+                           "send a purge status.")
