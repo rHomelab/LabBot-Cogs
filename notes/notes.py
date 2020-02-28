258eda55b23d2df930f54e5592f37682ee7a0ed0
Keir Davis <keirjdavis@gmail.com>
2020-02-28 10:54:38 +0000

    feat(notes): add storage settings

--- notes/notes.py
+++ notes/notes.py
@@ -1,5 +1,5 @@
 """discord red-bot notes"""
-from redbot.core import commands, Config
+from redbot.core import commands, Config, checks
 
 
 class NotesCog(commands.Cog):
@@ -8,3 +8,16 @@ class NotesCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.settings = Config.get_conf(self, identifier=127318281)
+
+        default_guild_settings = {
+            "notes": [],
+            "warnings": []
+        }
+
+        self.settings.register_guild(**default_guild_settings)
+
+    @commands.group(name="notes")
+    @commands.guild_only()
+    @checks.admin_or_permissions(manage_guild=True)
+    async def _notes(self, ctx: commands.Context):
+        pass
