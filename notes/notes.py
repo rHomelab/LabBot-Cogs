09f46a439f2d9afce827952f09037fb29a307671
Keir Davis <keirjdavis@gmail.com>
2020-02-28 10:58:57 +0000

    feat(notes): add status command

--- notes/notes.py
+++ notes/notes.py
@@ -1,4 +1,5 @@
 """discord red-bot notes"""
+import discord
 from redbot.core import commands, Config, checks
 
 
@@ -21,3 +22,24 @@ class NotesCog(commands.Cog):
     @checks.admin_or_permissions(manage_guild=True)
     async def _notes(self, ctx: commands.Context):
         pass
+
+    @_notes.command("status")
+    async def notes_status(self, ctx: commands.Context):
+        """Status of the cog.
+        The bot will display how many notes it has recorded
+        since it's inception.
+
+        Example:
+        - `[p]notes status`
+        """
+        data = discord.Embed(colour=(await ctx.embed_colour()))
+        data.title = "Notes Status"
+
+        async with self.settings.guild(ctx.guild).notes() as li:
+            data.add_field(name="Count", value=f"{len(li)} notes")
+
+        try:
+            await ctx.send(embed=data)
+        except discord.Forbidden:
+            await ctx.send("I need the `Embed links` permission to " +
+                           "send a notes status.")
