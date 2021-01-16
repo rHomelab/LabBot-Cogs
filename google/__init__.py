43ae2ccf1c080d58640abb468086e72ac137ee01
TheDevFreak <thedevfreak@gmail.com>
2021-01-16 15:17:00 +0000

    Add basic google link-sending cog

---
+++ google/__init__.py
@@ -0,0 +1,5 @@
+from .google import Google
+
+
+def setup(bot):
+    bot.add_cog(Google())
---
+++ google/google.py
@@ -0,0 +1,17 @@
+import random
+
+import discord
+from redbot.core import commands
+from redbot.core.utils.chat_formatting import pagify
+import urllib.parse
+
+class Google(commands.Cog):
+    """Google Command"""
+
+    def __init__(self):
+        pass
+
+    @commands.command()
+    async def google(self, ctx, *, query):
+        """Send a google link with provided query"""
+        await ctx.send("https://google.com/search?q="+urllib.parse.quote_plus(query))
