7ee074efc416120a87a1e7cc9541c4a416b22da5
Issy <isabella.szemeti@gmail.com>
2021-03-15 16:01:30 +0000

    chore(report): Var names

--- report/report.py
+++ report/report.py
@@ -157,7 +157,7 @@ class ReportCog(commands.Cog):
         bool_conversion = bool(supported_rules.index(rule.lower()))
 
         async with self.settings.guild(ctx.guild).channels() as channels:
-            data = filter(lambda x: x["id"] == str(ctx.channel.id), channels)
+            data = filter(lambda c: c["id"] == str(ctx.channel.id), channels)
             if data:
                 next(data)["reports"] = bool_conversion
             else:
@@ -193,7 +193,7 @@ class ReportCog(commands.Cog):
         bool_conversion = bool(supported_rules.index(rule.lower()))
 
         async with self.settings.guild(ctx.guild).channels() as channels:
-            data = filter(lambda x: x["id"] == str(ctx.channel.id), channels)
+            data = filter(lambda c: c["id"] == str(ctx.channel.id), channels)
             if data:
                 next(data)["emergencies"] = bool_conversion
             else:
@@ -216,7 +216,7 @@ class ReportCog(commands.Cog):
     ) -> bool:
         """Checks that reports/emergency commands are enabled in the current channel"""
         async with self.settings.guild(ctx.guild).channels() as channels:
-            channel = filter(lambda x: channel["id"] == str(
+            channel = filter(lambda c: c["id"] == str(
                 ctx.channel.id), channels)
 
             if channel:
