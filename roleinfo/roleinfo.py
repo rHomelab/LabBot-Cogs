51034e3966a497285510329186131c61d1823e44
Issy <isabella.szemeti@gmail.com>
2021-03-16 11:57:38 +0000

    fix(roleinfo): Fix comparison

--- roleinfo/roleinfo.py
+++ roleinfo/roleinfo.py
@@ -20,19 +20,23 @@ class RoleInfoCog(commands.Cog):
         - `[p]roleinfo verified`
         - `[p]roleinfo @verified`
         """
-        role_check = is_mod(self.bot, ctx.author) or role.position >= ctx.author.roles[-1].position
+        role_check = is_mod(
+            self.bot, ctx.author) or role.position <= ctx.author.roles[-1].position
         if role_check:
             embed = await self.make_role_embed(role)
             await ctx.send(embed=embed)
 
     async def make_role_embed(self, role: discord.Role) -> discord.Embed:
         """Generate the role info embed"""
-        embed = discord.Embed(title=f"Role info", colour=role.colour, timestamp=role.created_at)
+        embed = discord.Embed(title=f"Role info",
+                              colour=role.colour, timestamp=role.created_at)
         embed.add_field(name="Name", value=role.name)
         embed.add_field(name="Members", value=len(role.members))
         embed.add_field(name="Hoist", value="Yes" if role.hoist else "No")
-        embed.add_field(name="Mentionable", value="Yes" if role.mentionable else "No")
-        embed.add_field(name="Position", value=len(role.guild.roles) - role.position)
+        embed.add_field(name="Mentionable",
+                        value="Yes" if role.mentionable else "No")
+        embed.add_field(name="Position", value=len(
+            role.guild.roles) - role.position)
         embed.add_field(name="ID", value=role.id)
         embed.set_footer(text="Created")
         return embed
