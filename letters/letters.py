1ce67909728e714d4c439fefff5f7dd5ecd67918
tigattack <10629864+tigattack@users.noreply.github.com>
2021-07-15 16:32:42 +0100

    refactor(letters): return on unsupported match

--- letters/letters.py
+++ letters/letters.py
@@ -24,30 +24,29 @@ class Letters(commands.Cog):
         # Ensure it doesn't contain any special chars, numbers, etc.
         regexp = re.compile(r'[^a-z0-9 ]')
         if regexp.search(input):
-            await ctx.send('This cog accepts only a-z, 0-9, and whitespace characters.')
+            return await ctx.send('This cog accepts only a-z, 0-9, and whitespace characters.')
 
-        else:
-            # Initialise letters var
-            letters = ''
+        # Initialise letters var
+        letters = ''
 
-            # For each char in input
-            for char in input:
+        # For each char in input
+        for char in input:
 
-                # Double space if char is space
-                if char == ' ':
-                    letters += '  '
+            # Double space if char is space
+            if char == ' ':
+                letters += '  '
 
-                # Convert to number emote if number
-                elif char.isdigit():
-                    letters += f"{nums[int(char)]} "
+            # Convert to number emote if number
+            elif char.isdigit():
+                letters += f"{nums[int(char)]} "
 
-                # Convert to regional indicator emote if letter
-                else:
-                    letters += f":regional_indicator_{char}: "
+            # Convert to regional indicator emote if letter
+            else:
+                letters += f":regional_indicator_{char}: "
 
-            # Replace =>3 spaces with two
-            letters = re.sub(' {3,}', '  ', letters)
+        # Replace =>3 spaces with two
+        letters = re.sub(' {3,}', '  ', letters)
 
-            # Define and send message
-            output = f"{letters}\n```{letters}```"
-            await ctx.send(output)
+        # Define and send message
+        output = f"{letters}\n```{letters}```"
+        await ctx.send(output)
