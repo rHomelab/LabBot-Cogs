3128d7d4a5273236b3456caa3c7e8c3f51ed2932
tigattack <10629864+tigattack@users.noreply.github.com>
2021-07-15 14:05:56 +0100

    feat(letters): Initial cog creation

---
+++ letters/letters.py
@@ -0,0 +1,45 @@
+import re
+
+import discord
+from redbot.core import commands
+from redbot.core.utils.chat_formatting import pagify
+
+
+class Letters(commands.Cog):
+    """Letters cog"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def letters(self, ctx):
+        """Outputs large emote letters (\"regional indicators\") based on input text.
+        Accepts A-Z only.
+
+        Example:
+        - `[p]letters I'd like this text as emotes`
+        """
+
+        # Grab message content
+        input = ctx.Message.content
+
+        # Ensure it doesn't contain any special chars, numbers, etc.
+        regexp = re.compile(r'[^a-zA-Z ]')
+        if regexp.search(input):
+            await ctx.send('This cog accepts only A-Z and whitespace characters.')
+
+        else:
+            # Initialise letters var
+            letters=''
+            # For each char in input
+            for letter in input:
+                # Add double space if char is space
+                if letter == ' ':
+                    letters+='  '
+                # Else add letter as regional indicator emote
+                else: 
+                    letters+=f":regional_indicator_{letter}: "
+            
+            # Define and send message
+            msg = f"{letters}\n`{letters}`"
+            await ctx.send(msg)
