1158f9efa9f5cfd511b54a150dc02b0a078fd56d
tigattack <10629864+tigattack@users.noreply.github.com>
2021-07-15 15:00:02 +0100

    feat(letters): Add number support

--- letters/letters.py
+++ letters/letters.py
@@ -13,33 +13,58 @@ class Letters(commands.Cog):
 
     @commands.command()
     async def letters(self, ctx, *, msg):
-        """Outputs large emote letters (\"regional indicators\") based on input text.
-        Accepts A-Z only.
+        """Outputs large emote letters (\"regional indicators\") from input text.
+        Accepts A-Z, 0-9, and whitespace only.
 
         Example:
-        - `[p]letters I'd like this text as emotes`
+        - `[p]letters I'd like this text as emotes 123`
         """
 
         # Grab message content
         input = msg.lower()
 
+        # Define regex searches for letters and numbers
+        reletter = re.compile(r'[a-zA-Z]')
+        renumber = re.compile(r'[0-9]')
+
+        # Define numbers -> emotes dict
+        nums = {
+            0 : ':zero:',
+            1 : ':one:',
+            2 : ':two:',
+            3 : ':three:',
+            4 : ':four:',
+            5 : ':five:',
+            6 : ':six:',
+            7 : ':seven:',
+            8 : ':eight:',
+            9 : ':nine:'
+        }
+
         # Ensure it doesn't contain any special chars, numbers, etc.
-        regexp = re.compile(r'[^a-zA-Z ]')
+        regexp = re.compile(r'[^a-zA-Z0-9 ]')
         if regexp.search(input):
-            await ctx.send('This cog accepts only A-Z and whitespace characters.')
+            await ctx.send('This cog accepts only A-Z, 0-9, and whitespace characters.')
 
         else:
             # Initialise letters var
-            letters=''
+            letters = ''
+
             # For each char in input
-            for letter in input:
-                # Add double space if char is space
-                if letter == ' ':
-                    letters+='  '
-                # Else add letter as regional indicator emote
-                else: 
-                    letters+=f":regional_indicator_{letter}: "
-            
+            for char in input:
+
+                # Double space if char is space
+                if char == ' ':
+                    letters += '  '
+
+                # Convert to regional indicator emote if letter
+                elif reletter.search(char):
+                    letters += f":regional_indicator_{char}: "
+
+                # Convert to number emote if number
+                elif renumber.search(char):
+                    letters += f"{nums[char]} "
+
             # Define and send message
             output = f"{letters}\n`{letters}`"
             await ctx.send(output)
