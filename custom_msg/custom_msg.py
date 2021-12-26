ff9cb2b89a99819e5585e0c3b03d6aea23a88daf
Issy Szemeti <48881813+issy@users.noreply.github.com>
2021-12-26 13:39:10 +0000

    Custom message cog (#182)
    * feat(custom-msg): Added custommessage cog
    
    * chore(custom-msg): General tidy up
    
    * chore(custom-msg): Renamed files
    
    * chore(custom_msg): Renamed cog in info
    
    * fix(custom_msg): Compare content, not message
    
    * fix(custom_msg):Append string, fix message format
    
    * fix(custom_msg) Improved description flow
    
    * feat(custom-msg): Addesession handler
    
    * fix(custom-msg) Added loop condition
    
    * fix(custom-msg): Fixed callback case handling
    
    * chore(custom_msg): Linting
    
    * chore(custom_msg): Added debug lines
    
    * fix(custom_msg): return title
    
    * feat(custom_msg): Added mixed builder
    
    * fix(custom_msg): Improve user messages
    
    * fix(custom_msg): Remove embed/content on edit
    
    * fix(custom_msg): Kwarg name
    
    * docs(custom_msg): Added docs for custom msg
    
    * WIP
    
    * feat(custom_msg): Add ability to remove existing message attrs
    
    * fix(custom_msg): Payload check
    
    * refactor(custom_msg): Make method abstract
    
    * refactor(custom_msg): Remove method implementation
    
    * refactor(custom_msg): Linting
    
    * Update README.md
    
    Co-authored-by: tigattack <10629864+tigattack@users.noreply.github.com>
    
    * Update custom_msg/info.json
    
    Co-authored-by: tigattack <10629864+tigattack@users.noreply.github.com>
    
    Co-authored-by: Issy <issy@issy.dev>
    Co-authored-by: tigattack <10629864+tigattack@users.noreply.github.com>

---
+++ custom_msg/custom_msg.py
@@ -0,0 +1,59 @@
+import asyncio
+from typing import Optional
+
+import discord
+from redbot.core import checks, commands
+
+from .interactive_session import InteractiveSession, SessionCancelled, make_session
+
+
+class CustomMsgCog(commands.Cog):
+    @checks.mod()
+    @commands.guild_only()
+    @commands.group(name="msg")
+    async def msg_cmd(self, ctx: commands.Context):
+        pass
+
+    @msg_cmd.command(name="create", aliases=["send"])
+    async def msg_create(self, ctx: commands.Context, channel: Optional[discord.TextChannel] = None):
+        if channel is None:
+            channel = ctx.channel
+
+        try:
+            payload = await make_session(ctx)
+        except asyncio.TimeoutError:
+            return await ctx.send("Took too long to respond - exiting...")
+        except SessionCancelled:
+            return await ctx.send("Exiting...")
+
+        message = await channel.send(**payload)
+        await ctx.send(f"Message sent.\nFor future reference, the message ID is {message.channel.id}-{message.id}")
+
+    @msg_cmd.command(name="edit")
+    async def msg_edit(self, ctx: commands.Context, message: discord.Message):
+        if message.author != ctx.me:
+            return await ctx.send("You must specify a message that was sent by the bot.")
+
+        try:
+            payload = await make_session(ctx)
+        except asyncio.TimeoutError:
+            return await ctx.send("Took too long to respond - exiting...")
+        except SessionCancelled:
+            return await ctx.send("Exiting...")
+
+        payload = {key: val for key, val in payload.items() if val is not None}
+
+        if not payload.get("content") and message.content:
+            if not await InteractiveSession(ctx).get_boolean_answer(
+                "The original message has message content, but you have not specified any. Would you like to keep the original content?"
+            ):
+                payload.update({"content": ""})
+
+        if not payload.get("embed") and message.embeds:
+            if not await InteractiveSession(ctx).get_boolean_answer(
+                "The original message has an embed, but you have not specified one. Would you like to keep the original embed?"
+            ):
+                payload.update({"embed": None})
+
+        await message.edit(**payload)
+        await ctx.send("Message edited.")
