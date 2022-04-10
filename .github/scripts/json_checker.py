e6db382777f31229747d80f5ac15cf9b050c415c
issy <issy@issy.dev>
2022-04-10 20:30:12 +0100

    refactor(json_checker): Clean up json validation script

--- .github/scripts/json_checker.py
+++ .github/scripts/json_checker.py
@@ -22,11 +22,27 @@ def get_json(filename: str) -> Union[dict, list]:
         return json.load(f)
 
 
-def validate(schema_name: str, filename: str):
+def validate(schema_name: str, filename: str) -> bool:
     """Validates a json file based on a schema"""
-    schema = get_json(schema_name)
-    json_file = get_json(filename)
-    fastjsonschema.validate(schema, json_file)
+    try:
+        schema = get_json(schema_name)
+        json_file = get_json(filename)
+        fastjsonschema.validate(schema, json_file)
+        return True
+    except fastjsonschema.exceptions.JsonSchemaValueException as error:
+        print(error)
+
+        if error.rule == "additionalProperties" and not error.rule_definition:
+            error_keys, msg_bounds = list_from_str(error.message)
+            for key in error_keys:
+                line, col = get_key_pos(filename, key)
+                message = f"{error.message[:msg_bounds[0] + 1]}{key}{error.message[msg_bounds[1] - 1:]}"
+                print(format_output(level="error", file=filename, line=line, col=col, message=message))
+        else:
+            key_name = error.path[1]
+            line, col = get_key_pos(filename, key_name)
+            print(format_output(level="warning", file=filename, line=line, col=col, message=error.message))
+            return False
 
 
 def get_key_pos(filename: str, key: str) -> Tuple[int, int]:
@@ -34,12 +50,12 @@ def get_key_pos(filename: str, key: str) -> Tuple[int, int]:
     reg_match = re.compile(f'"{key}"\\s?:')
     with open(filename, "r") as f:
         lines = f.read().split("\n")
-    for i, line in enumerate(lines):
+    for i, line in enumerate(lines, start=1):
         match = reg_match.search(line)
         if not match:
             continue
         span = match.span()
-        return i + 1, span[0] + 1
+        return i, span[0] + 1
     raise Exception(f"could not get position of key: {key}")
 
 
@@ -54,28 +70,15 @@ def list_from_str(set_str: str) -> Tuple[List[str], Tuple[int, int]]:
 
 
 def main() -> int:
-    has_errors = False
+    validation_success = []
+    for file_pattern, schema_path in {
+            "info.json": ".github/actions/check_json/repo.json",
+            "*/info.json": ".github/actions/check-json/cog.json"
+    }.items():
+        for filename in glob(file_pattern):
+            validation_success.append(validate(schema_path, filename))
 
-    for iterable, schema_path in (("info.json", ".github/schemas/repo.json"), ("*/info.json", ".github/schemas/cog.json")):
-        for filename in glob(iterable):
-            try:
-                validate(schema_path, filename)
-            except fastjsonschema.exceptions.JsonSchemaValueException as error:
-                has_errors = True
-                print(error)
-
-                if error.rule == "additionalProperties" and not error.rule_definition:
-                    error_keys, msg_bounds = list_from_str(error.message)
-                    for key in error_keys:
-                        line, col = get_key_pos(filename, key)
-                        message = f"{error.message[:msg_bounds[0] + 1]}{key}{error.message[msg_bounds[1] - 1:]}"
-                        print(format_output(level="error", file=filename, line=line, col=col, message=message))
-                else:
-                    key_name = error.path[1]
-                    line, col = get_key_pos(filename, key_name)
-                    print(format_output(level="warning", file=filename, line=line, col=col, message=error.message))
-
-    return int(has_errors)
+    return int(not all(validation_success))
 
 
 if __name__ == "__main__":
