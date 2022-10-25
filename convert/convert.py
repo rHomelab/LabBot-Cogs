671b56b94b0ec55708f80fa20bba8d0902ff95b8
Leon Bowie <leon@bowie-co.nz>
2022-10-26 02:15:50 +1300

    move to gnu units

--- convert/convert.py
+++ convert/convert.py
@@ -1,16 +1,13 @@
 import discord
-from pint import UnitRegistry
 from redbot.core import commands
+import subprocess
 
 
 class Convert(commands.Cog):
     """Convert related commands."""
 
-    def __init__(self):
-        self.__ureg = UnitRegistry()
-
     @commands.command()
-    async def convert(self, ctx, *unit):
+    async def convert(self, ctx, conversion: str):
         """Convert from different kinds of units to other units using pint
 
         Example:
@@ -19,17 +16,26 @@ class Convert(commands.Cog):
         - `[p]convert 5in + 5ft to cm`
         """
 
+        arg1, arg2 = conversion.split(" to ")
+        
         try:
-            src, dst = " ".join(unit).split(" to ")
-            question = self.__ureg(src)
-            answer = question.to(dst)
-        except Exception:
-            error_embed = discord.Embed(
-                title="Error",
-                description=f"Unable to convert `{' '.join(unit)}`",
-                colour=await ctx.embed_colour(),
-            )
-            await ctx.send(embed=error_embed)
+            result = await ctx.bot.loop.run_in_executor(None, subprocess.check_output, ["units", arg1, arg2])
+        except subprocess.CalledProcessError as e:
+            error = e.output.decode("utf-8")
+            # grab the first line for the error type
+            error_type = error.splitlines()[0]
+            embed = discord.Embed(title="Error", description=f"Error when converting `{conversion}`\n{error_type}", color=0xff0000)
         else:
-            msg = f"{question.to_compact()} is {answer.to_compact()}"
-            await ctx.send(msg)
+            # the result is line 1
+            result = result.decode("utf-8").splitlines()[0]
+            # remove whitespace at the start and end
+            result = result.strip()
+            # check if first line has a * or /
+            if "*" in result or "/" in result:
+                # remove the character
+                result = result[1:]
+            # create embed
+            embed = discord.Embed(title="Convert", description=f"`{conversion}`\n{result}", color=0x00ff00)
+
+        await ctx.send(embed=embed)
+
