d69fefd6cc9966706129d39a7f0de5575bb8570a
Leon Bowie <31436575+Darkflame72@users.noreply.github.com>
2022-11-07 23:17:32 +1300

    cleanup error handling

--- convert/convert.py
+++ convert/convert.py
@@ -1,22 +1,36 @@
 import discord
 from redbot.core import commands
 import subprocess
+import re
 
 
 class Convert(commands.Cog):
     """Convert related commands."""
 
+    def __init__(self, bot):
+        self.bot = bot
+        self.decimal = re.compile(r'\d+')
+
+
     @commands.command()
     async def convert(self, ctx, conversion: str):
         """Convert from different kinds of units to other units using pint
-
         Example:
         - `[p]convert <from> to <to>`
         - `[p]convert 23cm to in`
         - `[p]convert 5in + 5ft to cm`
         """
 
-        arg1, arg2 = conversion.split(" to ")
+        if " to " not in conversion:
+            await ctx.send_help()
+            return
+
+        arg1, end_unit = conversion.split(" to ")
+
+        amount = re.search(self.decimal, arg1).group()
+        unit = arg1.replace(amount, "").strip()
+
+        arg1 = f"{amount} {unit}"
         
         try:
             result = await ctx.bot.loop.run_in_executor(None, subprocess.check_output, ["units", arg1, arg2])
@@ -32,10 +46,9 @@ class Convert(commands.Cog):
             result = result.strip()
             # check if first line has a * or /
             if "*" in result or "/" in result:
-                # remove the character
+                # remove the first character
                 result = result[1:]
             # create embed
-            embed = discord.Embed(title="Convert", description=f"`{conversion}`\n{result}", color=0x00ff00)
+            embed = discord.Embed(title="Convert", description=f"`{conversion}`\n`{result.strip()}{arg2}`", color=0x00ff00)
 
         await ctx.send(embed=embed)
-
