38496c776727183676a976dd1e5b91ba3f80d477
tigattack <10629864+tigattack@users.noreply.github.com>
2023-02-24 17:01:42 +0000

    markov: get end user data statement

--- markov/__init__.py
+++ markov/__init__.py
@@ -1,4 +1,8 @@
+from redbot.core.utils import get_end_user_data_statement
+
 from .markov import Markov
+
+__red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
 async def setup(bot):
     bot.add_cog(Markov(bot))
