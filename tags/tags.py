37a6360b91ee6928677c429b9f4e14fed07b0b3a
portalBlock <portalBlock@users.noreply.github.com>
2023-05-07 23:14:43 -0500

    Add: Tag editing, tag deletion perms check, tag transfer, alias creation and deletion, and implementation messages.

--- tags/tags.py
+++ tags/tags.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 
 import discord
+from discord import Guild
 from redbot.core import Config
 from redbot.core.bot import Red
 from redbot.core import commands
+from redbot.core.utils.mod import is_mod_or_superior
 
 
 class TagCog(commands.Cog):
@@ -14,7 +16,7 @@ class TagCog(commands.Cog):
         self.config = Config.get_conf(self, identifier=128986274420752384002)
 
         default_guild_config = {
-            "log": {
+            "log": {  # TODO: The log booleans are not respected right now. Everything is logged.
                 "transfers": True,
                 "uses": True
             },
@@ -106,6 +108,7 @@ class TagCog(commands.Cog):
 
     @_tag.command(name="stats")
     async def _stats(self, ctx: commands.Context, member: discord.Member):
+        await ctx.send("Stats reporting has not yet been implemented. Don't worry, we're tracking the stats though!")
         if not member:
             # TODO Return general stats
             pass
@@ -116,6 +119,7 @@ class TagCog(commands.Cog):
 
     @_tag.command(name="info")
     async def _info(self, ctx: commands.Context, tag: str):
+        await ctx.send("Info reporting has not yet been implemented.")
         if not tag:
             # TODO Error
             pass
@@ -124,17 +128,28 @@ class TagCog(commands.Cog):
             pass
 
     @_tag.command(name="edit")
-    async def _edit(self, ctx: commands.Context, tag: str, *content: str):
-        # TODO: Check if owner and edit the tag if so
-        pass
+    async def _edit(self, ctx: commands.Context, tag: str, *, content: str):
+        async with self.config.guild(ctx.guild).tags() as tags:
+            if tag in tags:
+                to = tags[tag]
+                if not to["owner"] == ctx.author.id and not is_mod_or_superior(self.bot, ctx.author):
+                    await ctx.send("Sorry, you're not the tag owner and you don't have permissions to do that.")
+                else:
+                    to["content"] = content
+                    await ctx.send("Tag successfully updated!")
+            else:
+                await ctx.send("That's not a tag!")
 
     @_tag.command(name="delete")
     async def _delete(self, ctx: commands.Context, tag: str):
-        # TODO: Check if owner/mod+
         async with self.config.guild(ctx.guild).tags() as tags:
             if tag in tags:
-                del tags[tag]
-                await ctx.send("Tag successfully deleted!")
+                to = tags[tag]
+                if not to["owner"] == ctx.author.id and not is_mod_or_superior(self.bot, ctx.author):
+                    await ctx.send("Sorry, you're not the tag owner and you don't have permissions to do that.")
+                else:
+                    del tags[tag]
+                    await ctx.send("Tag successfully deleted!")
             else:
                 await ctx.send("That's not a tag!")
 
@@ -145,8 +160,22 @@ class TagCog(commands.Cog):
 
     @_tag.command(name="transfer")
     async def _transfer(self, ctx: commands.Context, tag: str, member: discord.Member):
-        # TODO: Check if owner or mod+ and set owner to member then make the relevant ownership transfer entry
-        pass
+        async with self.config.guild(ctx.guild).tags() as tags:
+            if tag in tags:
+                to = tags[tag]
+                if not to["owner"] == ctx.author.id and not is_mod_or_superior(self.bot, ctx.author):
+                    await ctx.send("Sorry, you're not the tag owner and you don't have permissions to do that.")
+                else:
+                    curr_owner = to["owner"]
+                    new_owner = member.id
+                    if "transfers" not in to:
+                        to["transfers"] = []
+                    to["transfers"].append({"from": curr_owner,
+                                            "to": new_owner, "time": int(datetime.utcnow().timestamp())})
+                    to["owner"] = new_owner
+                    await ctx.send("Tag successfully transferred!")
+            else:
+                await ctx.send("That's not a tag!")
 
     @_tag.group(name="alias")
     async def _alias(self, ctx: commands.Context, alias: str, tag: str):
@@ -154,10 +183,66 @@ class TagCog(commands.Cog):
 
     @_alias.command("create")
     async def _alias_create(self, ctx: commands.Context, alias: str, tag: str):
-        # TODO See if the tag (or alias) exists and create the alias if not
-        pass
+        to, al, tag_proper, alias_proper = self.get_tag_or_alias(alias, ctx.guild)
+        if tag_proper:
+            await ctx.send("That's already a tag!")
+            return
+        elif alias_proper:
+            await ctx.send("That's already an alias!")
+        else:
+            async with self.config.guild(ctx.guild).aliases() as aliases:
+                aliases[alias] = {
+                    "creator": ctx.author.id,
+                    "created": int(datetime.utcnow().timestamp()),
+                    "tag": tag,
+                    "uses": []
+                }
+                await ctx.send("Alias successfully created!")
 
     @_alias.command("delete")
     async def _alias_delete(self, ctx: commands.Context, alias: str):
-        # TODO: Check if owner or mod+ and delete if so
-        pass
+        async with self.config.guild(ctx.guild).aliases() as aliases:
+            if alias in aliases:
+                a = aliases[alias]
+                if not a["owner"] == ctx.author.id and is_mod_or_superior(self.bot, ctx.author):
+                    await ctx.send("Sorry, you're not the alias owner and you don't have permissions to do that.")
+                    return
+                else:
+                    del aliases[alias]
+                    await ctx.send("Alias deleted successfully!")
+
+    def get_tag_or_alias(self, tag: str, guild: Guild) -> (object, [], bool, bool):
+        """Searches for a tag or alias based on the value provided.
+
+        If the provided query is a tag, it will return the tag and all aliases associated with it.
+
+        If the provided query is an alias, it will return itself as an alias plus the resolved tag.
+
+        If the provided query is a tag, the first returned boolean will be true.
+
+        If the provided query is an alias, the second returned boolean will be true.
+
+        All returned values are fully hydrated.
+
+        If no aliases are found, a zero-length array is returned.
+
+        """
+        t = None  # A single tag to return
+        a = []  # All aliases for a tag
+        tp = False
+        ap = False
+        tag_search = tag
+        async with self.config.guild(guild).aliases() as aliases:
+            if tag in aliases:
+                a = [aliases[tag]]  # Provided tag is only an alias
+                ap = True
+                tag_search = aliases[tag]["tag"]
+            else:
+                for alias in aliases:
+                    if alias["tag"] == tag:
+                        a.append(aliases[tag])
+        async with self.config.guild(guild).tags() as tags:
+            if tag_search in tags:
+                tp = tag_search == tag
+                t = tags[tag_search]
+        return t, a, tp, ap
