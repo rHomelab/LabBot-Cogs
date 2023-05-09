b2ddb098d427596e74fe34b5a8af66f346789e6b
tigattack <10629864+tigattack@users.noreply.github.com>
2023-05-09 21:24:43 +0100

    markov: await add_cog, use typed redbot.core.bot.Red arg

--- markov/__init__.py
+++ markov/__init__.py
@@ -1,8 +1,9 @@
+from redbot.core.bot import Red
 from redbot.core.utils import get_end_user_data_statement
 
 from .markov import Markov
 
 __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
-async def setup(bot):
-    bot.add_cog(Markov(bot))
+async def setup(bot: Red):
+    await bot.add_cog(Markov(bot))
