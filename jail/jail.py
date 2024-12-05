e7f684a4549a216ec7eed3aad6ff4994308c9556
portalBlock <portalBlock@users.noreply.github.com>
2024-12-05 10:40:42 -0600

    Partial undo

--- jail/jail.py
+++ jail/jail.py
@@ -31,7 +31,7 @@ class JailCog(commands.Cog):
             if message.channel.category_id == cat_channel.id:
                 jailset = await self.config.get_jailset_by_channel(ctx, message.channel)
                 if jailset is not None:
-                    # await self.config.save_message_to_jail(ctx, jailset, message, int(datetime.utcnow().timestamp()))
+                    await self.config.save_message_to_jail(ctx, jailset, message, int(datetime.utcnow().timestamp()))
                     jailset.log_message(Message.new(ctx, message.id, int(datetime.utcnow().timestamp()),
                                                     message.author.id, False, 0, message.content, []))
 
