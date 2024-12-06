28a1f92ecd2c87f397ef6aa77ffa220e29b18466
portalBlock <portalBlock@users.noreply.github.com>
2024-12-05 20:06:34 -0600

    Shocker! More silent fails

--- jail/utils.py
+++ jail/utils.py
@@ -197,6 +197,8 @@ class JailConfigHelper(JailConfigHelperABC):
             with open(transcript_path, "wb") as file:
                 file.write(transcript_object.getbuffer())
 
+            await ctx.send(transcript_path)
+
     async def get_jail_by_user(self, ctx: commands.Context, user: discord.User) -> JailABC:
         async with self.config.guild(ctx.guild).jails() as jails:
             if str(user.id) in jails:
