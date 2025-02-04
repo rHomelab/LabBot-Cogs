b9cb68960481fc54395fc9d050463f965fb5bf8e
tigattack <10629864+tigattack@users.noreply.github.com>
2025-02-04 01:59:50 +0000

    chore: add dev bot creation script

---
+++ create_dev_bot.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash -e
+
+# This aligns with the VSCode launch.json config:
+INSTANCE_NAME="RedBot_dev_homelab"
+
+echo "This script assumes you have python3 and pip installed on your system. If you don't, please install them before continuing."
+
+read -p "Enter your bot token: " TOKEN
+read -p "Enter your bot prefix [!]: " PREFIX
+PREFIX=${PREFIX:-"!"}
+
+is_venv_sourced=$(python3 -c 'import sys;print(sys.prefix != sys.base_prefix)')
+if [ "$is_venv_sourced" = "True" ]; then
+    echo "You are already in a virtual environment. If you are not currently using the venv you wish to use for RedBot, please deactivate it."
+    read -p "Do you want to continue? [y/N]: " CONTINUE
+    if [ "$CONTINUE" != "y" ]; then
+        exit 1
+    fi
+fi
+
+echo "Creating and activating virtual environment..."
+python3 -m venv venv
+source venv/bin/activate
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Creating RedBot instance..."
+redbot-setup --instance-name "$INSTANCE_NAME" --no-prompt --data-path .red_data
+redbot "$INSTANCE_NAME" --edit --no-prompt --token "$TOKEN" --prefix "$PREFIX"
+
+echo
+echo "Bot setup complete. You can now run the bot using the command 'redbot $INSTANCE_NAME' or by using the launch.json configuration in VSCode for debugging."
+echo
+echo "Run '${PREFIX}addpath $(dirname $(realpath "$0"))' in a chat with the bot to add this repository to the bot's list of cog paths."
