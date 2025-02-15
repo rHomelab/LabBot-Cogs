2fffcd69c2d4136f3db73dbf6937db68beb53086
ripple <github@ripple.contact>
2025-02-15 00:20:41 +0000

    refactor(convert): refactor convert cog to comply with ci

--- convert/convert.py
+++ convert/convert.py
@@ -27,37 +27,44 @@ class Convert(commands.Cog):
             return
 
         arg1, end_unit = conversion.split(" to ")
-
-        amount = decimal.search(arg1).group()
-        unit = arg1.replace(amount, "").strip()
-
-        arg1 = f"{amount} {unit}"
-
-        try:
-            result = await ctx.bot.loop.run_in_executor(None, subprocess.check_output, ["units", arg1, end_unit])
-        except subprocess.CalledProcessError as e:
-            error = e.output.decode("utf-8")
-            # grab the first line for the error type
-            error_type = error.splitlines()[0]
+        amount = decimal.search(arg1)
+        if amount is None:
             embed = discord.Embed(
                 title="Error",
-                description=f"Error when converting `{conversion}`\n{error_type}",
+                description="Error no decimal found",
                 color=discord.Color.red(),
             )
         else:
-            # the result is line 1
-            result = result.decode("utf-8").splitlines()[0]
-            # remove whitespace at the start and end
-            result = result.strip()
-            # check if first line has a * or /
-            if "*" in result or "/" in result:
-                # remove the first character
-                result = result[1:]
-            # create embed
-            embed = discord.Embed(
-                title="Convert",
-                description=f"`{conversion}`\n`{result.strip()}{end_unit}`",
-                color=discord.Color.green(),
-            )
+            amount_group = amount.group()
+            unit = arg1.replace(amount_group, "").strip()
+
+            arg1 = f"{amount_group} {unit}"
+
+            try:
+                result = await ctx.bot.loop.run_in_executor(None, subprocess.check_output, ["units", arg1, end_unit])
+            except subprocess.CalledProcessError as e:
+                error = e.output.decode("utf-8")
+                # grab the first line for the error type
+                error_type = error.splitlines()[0]
+                embed = discord.Embed(
+                    title="Error",
+                    description=f"Error when converting `{conversion}`\n{error_type}",
+                    color=discord.Color.red(),
+                )
+            else:
+                # the result is line 1
+                result = result.decode("utf-8").splitlines()[0]
+                # remove whitespace at the start and end
+                result = result.strip()
+                # check if first line has a * or /
+                if "*" in result or "/" in result:
+                    # remove the first character
+                    result = result[1:]
+                # create embed
+                embed = discord.Embed(
+                    title="Convert",
+                    description=f"`{conversion}`\n`{result.strip()}{end_unit}`",
+                    color=discord.Color.green(),
+                )
 
         await ctx.send(embed=embed)
