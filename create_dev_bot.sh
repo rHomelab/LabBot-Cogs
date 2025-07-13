5d894f83cd3df19f0c0c5c8f69dcca6095af4691
tigattack <10629864+tigattack@users.noreply.github.com>
2025-07-13 16:11:34 +0100

    fix(create_dev_bot): fix venv source

--- create_dev_bot.sh
+++ create_dev_bot.sh
@@ -27,7 +27,7 @@ fi
 
 echo "Creating and activating virtual environment..."
 python3 -m venv .venv
-source venv/bin/activate
+source .venv/bin/activate
 
 echo "Installing dependencies..."
 pip install -r requirements.txt
