cb868b89837642b5bbdfc88b965ff49ada1fbd7b
tigattack <10629864+tigattack@users.noreply.github.com>
2025-08-27 13:49:33 +0100

    feat(compile_cog_reqs): count requirements and cogs

--- .github/compile_cog_requirements.py
+++ .github/compile_cog_requirements.py
@@ -4,16 +4,18 @@ import json
 from glob import glob
 
 
-def fetch_requirements() -> set[str]:
+def fetch_requirements() -> tuple[set[str], int]:
     requirements = set()
+    cogs_with_requirements = 0
 
     for filename in glob("*/info.json"):
         with open(filename, "r") as fp:
             info = json.load(fp)
         if "requirements" in info:
             requirements.update(info["requirements"])
+            cogs_with_requirements += 1
 
-    return requirements
+    return requirements, cogs_with_requirements
 
 
 def write_requirements(requirements: set[str]):
@@ -22,6 +24,6 @@ def write_requirements(requirements: set[str]):
 
 
 if __name__ == "__main__":
-    all_requirements = fetch_requirements()
+    all_requirements, cog_count = fetch_requirements()
     write_requirements(all_requirements)
-    print("Compiled requirements")
+    print(f"Compiled {len(all_requirements)} requirements from {cog_count} cogs")
